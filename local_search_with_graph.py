bdf1577f3b7f7173e0f47c7551ad24b0cbfc9d6d
Olmo <olmichu33@gmail.com>
2024-05-07 17:44:00 +0200

    Cambios menores

--- local_search_with_graph.py
+++ local_search_with_graph.py
@@ -7,8 +7,8 @@ mean = 0
 std_dev = 5
 np.random.seed(0)
 # Generar n_points puntos aleatorios de una normal de media 0 y desviacion std de dimensión 2
-N = np.random.normal(mean, std_dev, size=(n_points, 2))
-# N = np.random.randn(0,20, size= (n_points,2))
+# N = np.random.normal(mean, std_dev, size=(n_points, 2))
+N = np.random.randint(0,200, size= (n_points,2))
 M_dim = 50
 X, data, Da_m = MD_problem_heuristic(N, M_dim, 1, verbose = False)
 local_search_MA_animated(X, Da_m, N, n_points-np.sum(X), np.sum(X)-1, verbose = True)
